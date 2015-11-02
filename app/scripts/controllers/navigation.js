52d55739779013e00a52bffd93b4b0cb5217c5de
Michael Räss <raess.michael@gmail.com>
2015-11-02 19:16:17 +0100

    Added font-awesome and navigation controller

---
+++ app/scripts/controllers/navigation.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vaTraApp.controller:NavigationCtrl
+ * @description
+ * # NavigationCtrl
+ * Controller of the vaTraApp
+ */
+angular.module('vaTraApp')
+    .controller('NavigationCtrl', function ($scope, $location) {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        $scope.items = [
+            {
+                name: 'Schema',
+                link: 'schema'
+            },
+            {
+                name: 'Sicherheit',
+                link: 'security'
+            },
+            {
+                name: 'Algorithmen',
+                link: 'algos'
+            },
+            {
+                name: 'Statistiken',
+                link: 'stats'
+            },
+            {
+                name: 'API',
+                link: 'api'
+            }
+        ];
+
+        $scope.isActive = function (link) {
+            return '/' + link === $location.path();
+        }
+    });
