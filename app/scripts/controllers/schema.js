74502a7d863f6c24511abc955f5298387824f564
dave <dave@sidefyn.ch>
2015-11-20 13:24:20 +0100

    Fix Dashboard, Login & Shema

--- app/scripts/controllers/schema.js
+++ app/scripts/controllers/schema.js
@@ -8,7 +8,7 @@
  * Controller of the vaTraApp
  */
 angular.module('vaTraApp')
-  .controller('SchemaCtrl', function (JSONEditor) {
+  .controller('SchemaCtrl', function () {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
