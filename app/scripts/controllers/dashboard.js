a2d1b46cc9bace0cc3de681239e3c20c39d6771b
Michael Räss <raess.michael@gmail.com>
2015-11-30 20:40:42 +0100

    Worked on dashboard

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -8,58 +8,66 @@
  * Controller of the vaTraApp
  */
 angular.module('vaTraApp')
-  .controller('DashboardCtrl', function ($resource, $scope, CsrfService, $http) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    .controller('DashboardCtrl', function ($resource, $scope, CsrfService, $http) {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
 
-    var appResources = function (headers) {
-      if (headers !== undefined) {
-        return $resource('http://localhost:8080/rest/secure/app', {}, {
-          post: {method: 'POST', headers: headers, isArray: false}
-        });
-      } else {
-        return $resource('http://localhost:8080/rest/secure/app', {}, {
-          get: {method: 'GET', cache: false, isArray: true},
-          options: {method: 'OPTIONS', cache: false}
-        });
-      }
-    };
+        var appResources = function (headers) {
+            if (headers !== undefined) {
+                return $resource('http://localhost:8080/rest/secure/app', {}, {
+                    post: {method: 'POST', headers: headers, isArray: false}
+                });
+            } else {
+                return $resource('http://localhost:8080/rest/secure/app', {}, {
+                    get: {method: 'GET', cache: false, isArray: true},
+                    options: {method: 'OPTIONS', cache: false}
+                });
+            }
+        };
 
 
-    var getApps = function () {
-      appResources().get().$promise.then(function (data) {
-        console.log('GET /rest/secure/app returned: ', data);
-        var apps = [];
-        data.forEach(function (record) {
-          apps.push({
-            name: record.name
-          });
-        });
+        var getApps = function () {
+            appResources().get().$promise.then(function (data) {
+                console.log('GET /rest/secure/app returned: ', data);
+                var apps = [];
+                data.forEach(function (record) {
+                    apps.push({
+                        name: record.name
+                    });
+                });
 
-        $scope.apps = apps;
+                $scope.apps = apps;
 
-      }).catch(function (response) {
-        handleError(response);
-      });
-    };
+            }).catch(function (response) {
+                handleError(response);
+            });
+        };
 
-    $scope.saveApp = function () {
-      CsrfService.addResourcesCsrfToHeaders(appResources().options, $http.defaults.headers.post).then(function (headers) {
-        appResources(headers).post({name: 'test'}).$promise.then(function (response) {
-          console.log('POST /rest/secure/app returned: ', response);
-          console.info('You might want to check the server logs to see that the POST has been handled!');
-        }).catch(function (response) {
-          handleError(response);
-        });
-      });
-    };
+        $scope.saveApp = function (appName) {
+            if (appName === '') {
+                alert('App-Name darf nicht leer sein!');
+                return;
+            }
 
-    getApps();
+            CsrfService.addResourcesCsrfToHeaders(appResources().options, $http.defaults.headers.post).then(function (headers) {
+                appResources(headers).post({
+                    name: appName,
+                    scheme: ''
+                }).$promise.then(function (response) {
+                        console.log('POST /rest/secure/app returned: ', response);
+                        console.info('You might want to check the server logs to see that the POST has been handled!');
+                    }).catch(function (response) {
+                        handleError(response);
+                    });
+            });
+        };
 
-    var handleError = function (response) {
-      console.error('Something went wrong...', response);
-    };
-  });
+        getApps();
+
+        var handleError = function (response) {
+            console.error('Something went wrong...', response);
+        };
+    });
