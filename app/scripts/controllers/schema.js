27ebfb3a36ecce1d840646c0fcc23daeefc7747d
Tobias Schmoker <schmoker@novu.ch>
2015-12-09 19:09:54 +0100

    makes schemes saveable adds helpers in scss

--- app/scripts/controllers/schema.js
+++ app/scripts/controllers/schema.js
@@ -8,7 +8,8 @@
  * Controller of the vaTraApp
  */
 angular.module('vaTraApp')
-    .controller('SchemaCtrl', function () {
+    .controller('SchemaCtrl', function ($scope, $routeParams, $resource, CsrfService, $http) {
+
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
@@ -19,21 +20,40 @@ angular.module('vaTraApp')
         var container = document.getElementById("jsoneditor");
         var editor = new JSONEditor(container);
 
-        // set json
-        var json = {
-            "name": {
-                "min": 8,
-                "max": 10,
-                "required": true
-            },
-            "email": "VaTra.Regex.Email",
-            "payment_receiver": "VaTra.Payment.Receiver",
-            "payment_sender": "VaTra.Payment.Sender"
-        };
 
-        editor.set(json);
+          $scope.updateSchema = function () {
+              CsrfService.addResourcesCsrfToHeaders(schemaResources().options, $http.defaults.headers.post).then(function (headers) {
+                  schemaResources(headers).put({
+                      schema: editor.get()
+                  }).$promise.then(function () {
+                          toastr.success('Erfolgreich gespeichert');
+                      }).catch(function (response) {
+                          console.error('Something went wrong...', response);
+                      });
+              });
+          };
 
-        // get json
-        editor.get();
+          var schemaResources = function (headers) {
+              if (headers !== undefined) {
+                  return $resource('http://localhost:8080/rest/secure/app/' + $routeParams.appId + '/schema', {}, {
+                      put: {method: 'PUT', headers: headers, isArray: false}
+                  });
+              } else {
+                  return $resource('http://localhost:8080/rest/secure/app/' + $routeParams.appId + '/schema', {}, {
+                      get: {method: 'GET', cache: false, isArray: true},
+                      options: {method: 'OPTIONS', cache: false}
+                  });
+              }
+          };
+
+          schemaResources().get().$promise.then(function (data) {
+            console.log(data[0]);
+
+            if (data[0] != "")
+              editor.set(JSON.parse(data[0]).schema);
+
+          }).catch(function (response) {
+              console.error('Something went wrong...', response);
+          });
 
     });
