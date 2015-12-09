be459938fab02ccc7018670f69c2ad4d3933c47e
Michael Räss <raess.michael@gmail.com>
2015-12-09 20:26:16 +0100

    Changed url

--- vatra-php/index.php
+++ vatra-php/index.php
@@ -1,1 +1,19 @@
-<?phpheader('Access-Control-Allow-Origin: http://localhost:9000');header('Access-Control-Allow-Credentials: true');$_POST = json_decode(file_get_contents('php://input'), true);$_POST['apiKey'] = 'd33i7sn7gj62t4mdptsfe1pclt';$curl = curl_init();curl_setopt($curl, CURLOPT_URL,"http://localhost:8080/rest/open");curl_setopt($curl, CURLOPT_POST, 1);curl_setopt($curl, CURLOPT_POSTFIELDS, 'jsonParams=' . json_encode($_POST));curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);$rs = curl_exec($curl);echo $rs;curl_close($curl);
+<?php
+
+header('Access-Control-Allow-Origin: http://localhost:9000');
+header('Access-Control-Allow-Credentials: true');
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+$_POST['apiKey'] = 'd33i7sn7gj62t4mdptsfe1pclt';
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL,"http://api.vatra.kioh.ch/rest/open");
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, 'jsonParams=' . json_encode($_POST));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$rs = curl_exec($curl);
+echo $rs;
+
+curl_close($curl);
