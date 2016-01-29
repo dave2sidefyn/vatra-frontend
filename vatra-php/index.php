da2ffeb7bf4560393dc8e82fd3c237603512cd64
dave <dave@sidefyn.ch>
2016-01-29 13:09:01 +0100

    Demo anpassungen

--- vatra-php/index.php
+++ vatra-php/index.php
@@ -4,13 +4,13 @@ header('Access-Control-Allow-Origin: http://localhost:9000');
 header('Access-Control-Allow-Credentials: true');
 
 $_POST = json_decode(file_get_contents('php://input'), true);
-$_POST['VaTra.ApiKey'] = 'd33i7sn7gj62t4mdptsfe1pclt';
-$_POST['VaTra.Identification'] = 'ABC123456';
+$_POST['VaTra.ApiKey'] = '50d4d6ea-b126-48b6-81ab-7880b45a774c';
+$_POST['VaTra.Identification'] = $_POST['creditCardNumber'];
 $_POST['VaTra.Honeypot'] = '';
 
 $curl = curl_init();
 
-curl_setopt($curl, CURLOPT_URL,"http://localhost:8080/rest/open");
+curl_setopt($curl, CURLOPT_URL, "http://localhost:8080/rest/open");
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, 'jsonParams=' . json_encode($_POST));
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
